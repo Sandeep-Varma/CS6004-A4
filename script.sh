fe195ccb45233e501ec697caa3aee6d6a474c09e
Sandeep-Varma <dsv6799@gmail.com>
2024-04-30 13:52:01 +0530

    Added folder for class files without optimization

--- script.sh
+++ script.sh
@@ -1,16 +1,21 @@
 rm -f output.txt
-rm -rf *.class */*.class testcase sootOutput
+rm -rf *.class */*.class testcase sootOutput javaOutput
 
 javac -cp .:soot.jar PA4.java
 
+mkdir javaOutput
+
 for file in testcases/*.java
 do
+    testcase=$(basename $file)
     mkdir testcase
     cp $file testcase/Test.java
     cd testcase/
     javac -g Test.java
     cd ..
-    echo "####################################################################### $file"
+    mkdir javaOutput/${testcase%.*}
+    cp testcase/*.class javaOutput/${testcase%.*}/
+    echo "####################################################################### $testcase"
     java -cp .:soot.jar PA4 $file
     #  | grep -v "^Soot " >> ../output.txt
     rm -rf testcase
